f5d07a8987c484bf10e193d54371f541c138c30f
sajitha-tj <sajithaj@wso2.com>
2025-04-21 14:59:56 +0530

    Fix status code in the response

--- setup/clientServer/clientServer.go
+++ setup/clientServer/clientServer.go
@@ -1,6 +1,6 @@
 package main
 
-// This is a sample client server that handles the OAuth2 authorization code flow.
+// This is a sample client server that helps to handle the OAuth2 authorization code flow.
 // It listens for a callback from the authorization server and handles the redirect.
 // This server is not a complete implementation of an OAuth2 client, but rather used for testing purposes.
 // It is meant to be run alongside with the STS.
@@ -14,7 +14,6 @@ import (
 func main() {
 	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Auth callback received")
-		w.WriteHeader(http.StatusOK)
 
 		w.Header().Set("Content-Type", "application/json")
 
@@ -22,12 +21,14 @@ func main() {
 		// error handling
 		if r.URL.Query().Get("error") != "" {
 			// send json error response
+			w.WriteHeader(http.StatusUnauthorized)
 			responseBodyData = map[string]string{
 				"error":             r.URL.Query().Get("error"),
 				"error_description": r.URL.Query().Get("error_description"),
 			}
 		} else {
 			// success response: send all URL query params as JSON
+			w.WriteHeader(http.StatusOK)
 			for key, values := range r.URL.Query() {
 				if len(values) > 0 {
 					responseBodyData[key] = values[0]
